4c6d5f4bb1ee010b18af4bf7c9dffd058b27cc64
Omar Amiri <omar@openvelvet.com>
2015-04-26 20:09:02 -0700

    removed imageUploader

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
 
   has_one :profile, dependent: :destroy
 
-  mount_uploader :image, ImageUploader
+  
 
   def self.from_omniauth(auth)
       where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
